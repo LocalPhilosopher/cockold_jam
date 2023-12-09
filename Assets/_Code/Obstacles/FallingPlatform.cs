627cf65d05baef2540f6199912c32c5e907278f4
SweatyKeyboard <klimenkoff_v@mail.ru>
2023-12-09 14:47:37 +0300

    Falling platform

--- Assets/_Code/Obstacles/FallingPlatform.cs
+++ Assets/_Code/Obstacles/FallingPlatform.cs
@@ -8,13 +8,16 @@ namespace _Code.Obstacles
     public sealed class FallingPlatform : MonoBehaviour
     {
         [SerializeField] private float _stabilityDuration;
-        [SerializeField] private float _animationDuration;
+        [SerializeField] private float _shakingAnimationDuration;
+        [SerializeField] private float _fallingAnimationDuration;
 
+        private SpriteRenderer _spriteRenderer;
         private Collider2D _collider2d;
 
         private void Awake()
         {
             _collider2d = GetComponent<Collider2D>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
         private void OnCollisionEnter2D(Collision2D other)
@@ -27,14 +30,17 @@ namespace _Code.Obstacles
             await UniTask.Delay(TimeSpan.FromSeconds(_stabilityDuration));
 
             var sequence = DOTween.Sequence();
-            sequence.Append(transform.DORotate(new Vector3(0, 0, 5), _animationDuration / 3f));
-            sequence.Append(transform.DORotate(new Vector3(0, 0, -5), _animationDuration / 3f));
-            sequence.Append(transform.DORotate(new Vector3(0, 0, 0), _animationDuration / 3f));
+            sequence.Append(transform.DORotate(new Vector3(0, 0, 5), _shakingAnimationDuration / 3f));
+            sequence.Append(transform.DORotate(new Vector3(0, 0, -5), _shakingAnimationDuration / 3f));
+            sequence.Append(transform.DORotate(new Vector3(0, 0, 0), _shakingAnimationDuration / 3f));
             sequence.Play();
             await sequence.ToUniTask();
 
             _collider2d.enabled = false;
-            transform.DOMoveY(transform.position.y - 100, 1f);
+            sequence = DOTween.Sequence();
+            sequence.Append(transform.DOMoveY(transform.position.y - 10f, _fallingAnimationDuration));
+            sequence.Insert(0, _spriteRenderer.DOColor(Color.clear, _fallingAnimationDuration));
+
         }
     }
 }
