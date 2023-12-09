3ebe3f10d7db4e9f219031ea42991aff342396a9
LocalPhilosopher <rekunigato@gmail.com>
2023-12-09 15:58:18 +0300

    - add UISCENEManager

---
+++ Assets/_Code/UI/UISceneManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using _Code.Utils;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Code
+{
+    public class UISceneManager : SingletonMono<UISceneManager>
+    {
+        [SerializeField] private Image fadeImage;
+        
+        private void Start()
+        {
+            FadeOut();
+        }
+
+        public void FadeOut()
+        {
+        }
+
+        public void FadeIn(Action callback)
+        {
+            callback?.Invoke();
+        }
+    }
+}
